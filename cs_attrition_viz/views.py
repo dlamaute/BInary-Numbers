ea025a83297a8869d5498fe5a19b61ba3bfb663e
Diana Lamaute <dlamaute@mit.edu>
2022-09-15 19:43:09 -0400

    starting to add csv data retrieval functions

--- cs_attrition_viz/views.py
+++ cs_attrition_viz/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.db.models import F
 from cs_attrition_viz import models
 
+
+"""
+Actual views
+"""
 
 def cs_attrition_home(request):
 	return render(request, 'home.html', {})
@@ -13,6 +18,49 @@ def cs_attrition_population(request):
 
 def cs_attrition_salary(request):
 	return render(request, 'salary.html', {})
+
+
+"""
+Data retrieval methods
+"""
+
+# Columns used for the visualizations and those columns' corresponding source notes columns
+relevant_data_columns = ['year', 'num_employees_nsf', 'salary_nsf', 'num_recent_grad_employees', 'num_recent_grad_nonstem']
+relevant_source_columns = ['year', 'num_employees_nsf_source', 'salary_nsf_source', 'num_recent_grad_employees_source', 'num_recent_grad_nonstem_source']
+
+def source_data(modeladmin, request, queryset):
+	cs_women = CsWomen.objects.values('year', cs_women_num_employees_nsf=F('num_employees_nsf'), cs_women_salary_nsf=F('salary_nsf'), cs_women_num_recent_grad_employees=F('num_recent_grad_employees'), cs_women_num_recent_grad_nonstem=('num_recent_grad_nonstem'))
+	cs_men = CsMen.objects.values('year', cs_men_num_employees_nsf=F('num_employees_nsf'), cs_men_salary_nsf=F('salary_nsf'), cs_men_num_recent_grad_employees=F('num_recent_grad_employees'), cs_men_num_recent_grad_nonstem=('num_recent_grad_nonstem'))
+
+	if not request.user.is_advocacy_member:
+		raise PermissionDenied
+	metadata = queryset.model._meta
+	model = queryset.model
+	response = HttpResponse(
+		content_type='text/csv',
+		headers={'Content-Disposition': 'attachment; filename="cs_government_gender_data.csv"'},
+	)
+	csv_writer = csv.writer(response)
+	fields = [field.name for fields in metadata.fields]
+	csv_writer.writerow(fields)
+	for query_response_object in queryset:
+		csv_writer.writerow([get_attr(query_response_object, field) for field in fields])
+	return response
+
+
+def data_source_table(modeladmin, request, queryset):
+	metadata = queryset.model._meta
+	model = queryset.model
+	response = HttpResponse(
+		content_type='text/csv',
+		headers={'Content-Disposition': 'attachment; filename="cs_government_gender_data.csv"'},
+	)
+	csv_writer = csv.writer(response)
+	fields = [field.name for fields in metadata.fields]
+	csv_writer.writerow(fields)
+	for query_response_object in queryset:
+		csv_writer.writerow([get_attr(query_response_object, field) for field in fields])
+	return response
 
 
 def womens_data(request):
@@ -27,3 +75,20 @@ def mens_data(request):
 	# 	'men': men
 	# }
 	return JsonResponse(list(men), safe=False)
+
+
+"""
+Helper Functions
+"""
+def create_csv_from_db_table(modeladmin, request, queryset, relabel_conflicting_fields=False):
+	if not request.user.is_advocacy_member:
+		raise PermissionDenied
+	metadata = queryset.model._meta
+	model = queryset.model
+	response = HttpResponse(
+		content_type='text/csv',
+		headers={'Content-Disposition': 'attachment; filename="cs_government_gender_data.csv"'},
+	)
+	csv_writer.csv.writer(response)
+	if (relabel_conflicting_fields):
+
